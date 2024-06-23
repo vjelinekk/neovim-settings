c458d34fde0718af3ea6f37b6fbef7465f4ab819
vjelinekk <vojtech.jelinekk@gmail.com>
2024-06-23 03:32:49 +0200

    added github copilot

--- lua/vojta/packer.lua
+++ lua/vojta/packer.lua
@@ -82,4 +82,7 @@ return require('packer').startup(function(use)
     use {"akinsho/toggleterm.nvim", tag = '*', config = function()
         require("toggleterm").setup()
     end}
+    -- copilot
+    use {'github/copilot.vim', branch = 'release' }
+
 end)

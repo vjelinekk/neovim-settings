9ee82f3257522c05bc23c85ba8b60d8a6bd5b56a
vjelinekk <vojtech.jelinekk@gmail.com>
2024-09-06 14:44:51 +0200

    new plugins and gitignore update

---
+++ after/plugin/aerial.lua
@@ -0,0 +1,3 @@
+require("aerial").setup()
+
+vim.keymap.set("n", "<leader>fs", "<cmd>AerialNavToggle<CR>")
---
+++ after/plugin/fugitive.lua
@@ -0,0 +1,1 @@
+vim.api.nvim_set_keymap("n", "<leader>c", "<cmd>Gvdiffsplit!<CR>", {noremap = true, silent = true})

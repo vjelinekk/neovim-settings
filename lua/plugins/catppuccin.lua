c5e20b24283ea6cca7616fc6145448391600774c
vjelinekk <vojtech.jelinekk@gmail.com>
2025-02-08 03:44:59 +0100

    Java LSP

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,4 +1,4 @@
-return { 
+return {
     "catppuccin/nvim", name = "catppuccin", priority = 1000,
     config = function()
         require("catppuccin").setup()
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -26,29 +26,7 @@ return {
             lazygit:toggle()
         end
 
-        local normal_terminal = Terminal:new({
-            cmd = "zsh",
-            direction = "float",
-            float_opts = {
-                border = "double",
-            },
-            -- function to run on opening the terminal
-            on_open = function(term)
-                vim.cmd("startinsert!")
-                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
-            end,
-            -- function to run on closing the terminal
-            on_close = function(term)
-                vim.cmd("startinsert!")
-            end,
-        })
-
-        function _normal_terminal_toggle()
-            normal_terminal:toggle()
-        end
-
         vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = false, silent = true})
-        vim.api.nvim_set_keymap("n", "<C-\\>", "<cmd>lua _normal_terminal_toggle()<CR>", {noremap = false, silent = true})
     end
 }
 

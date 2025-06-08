7696644a65ff9f449e8d8670659072c7df622ab4
vjelinekk <vojtech.jelinekk@gmail.com>
2025-06-08 17:14:30 +0200

    MR conflicts

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,8 +2,36 @@ return {
     {
         'tpope/vim-fugitive',
         config = function ()
-            -- vim.api.nvim_set_keymap("n", "<leader>c", "<cmd>Gvdiffsplit!<CR>", {noremap = true, silent = true})
+            vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>Gvdiffsplit!<CR>", {noremap = true, silent = true})
             vim.api.nvim_set_keymap("n", "<leader>gb", "<cmd>Git blame<CR>", {noremap = true, silent = true})
+
+            -- Helper function to get buffer number matching fugitive path
+            local function get_fugitive_bufnum(side)
+                for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+                    local name = vim.api.nvim_buf_get_name(bufnr)
+                    if name:match("//" .. side .. "/") then
+                        return bufnr
+                    end
+                end
+                vim.notify("No fugitive buffer found for //" .. side, vim.log.levels.WARN)
+                return nil
+            end
+
+            -- Diffget from LEFT (//2)
+            vim.keymap.set("n", "<leader>gl", function()
+                local buf = get_fugitive_bufnum("2")
+                if buf then
+                    vim.cmd("diffget " .. buf)
+                end
+            end, { desc = "Diffget LEFT (//2)" })
+
+            -- Diffget from RIGHT (//3)
+            vim.keymap.set("n", "<leader>gr", function()
+                local buf = get_fugitive_bufnum("3")
+                if buf then
+                    vim.cmd("diffget " .. buf)
+                end
+            end, { desc = "Diffget RIGHT (//3)" })
         end
     },
     {
